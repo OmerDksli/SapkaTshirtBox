593faffabfa8535327b5f2c1f8b69d5518891bfe
ömer <mer@DESKTOP-STOKBJ0>
2023-05-18 13:45:46 +0300

    Add project files.

---
+++ OzSapkaTShirt/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using OzSapkaTShirt.Data;
+using OzSapkaTShirt.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace OzSapkaTShirt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationContext _context;
+        Order? order;
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index(string? id=null)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null)
+            {
+                order = _context.Orders.Where(o => o.UserId == userId && o.Status == 0)
+                .Include(o => o.OrderProducts).FirstOrDefault();
+                if (order != null)
+                    ViewData["TotalQuantity"] = order.OrderProducts.Sum(o => o.Quantity);
+                else
+                    ViewData["TotalQuantity"] = null;
+            }
+            else
+                ViewData["TotalQuantity"] = null;
+
+            return View(_context.Products.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OzSapkaTShirt/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OzSapkaTShirt.Data;
+using Microsoft.AspNetCore.Identity;
+using OzSapkaTShirt.Models;
+namespace OzSapkaTShirt
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ApplicationContext context;
+
+            builder.Services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext") ?? throw new InvalidOperationException("Connection string 'ApplicationContext' not found.")));
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => { options.SignIn.RequireConfirmedAccount = false; options.Password.RequireNonAlphanumeric = false; })
+                .AddEntityFrameworkStores<ApplicationContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+                        app.UseAuthentication();;
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+              name: "areas",
+              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            context = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope().ServiceProvider.GetService<ApplicationContext>();
+            context.Database.Migrate();
+
+
+            app.Run();
+        }
+    }
+}
